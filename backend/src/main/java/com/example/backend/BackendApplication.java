b18953c79e2640c736d607b7b0c1f3f60c07bdbf
Haemin Kim <haemin1230@gmail.com>
2025-08-07 10:44:21 +0900

    :wrench: (#3) db 연결 및 설정 파일 추가

--- backend/src/main/java/com/example/backend/BackendApplication.java
+++ backend/src/main/java/com/example/backend/BackendApplication.java
@@ -1,5 +1,6 @@
 package com.example.backend;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BackendApplication {
 
 	public static void main(String[] args) {
+
+		Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+
+		System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
+		System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
+		System.setProperty("DB_NAME", dotenv.get("DB_NAME"));
+		System.setProperty("DB_HOST", dotenv.get("DB_HOST"));
+		System.setProperty("DB_PORT", dotenv.get("DB_PORT"));
+
 		SpringApplication.run(BackendApplication.class, args);
 	}
 
