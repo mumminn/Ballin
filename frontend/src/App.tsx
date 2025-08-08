ae694009bfd2df8bfa229a8931015c11f36bcc0e
Haemin Kim <haemin1230@gmail.com>
2025-08-08 17:44:07 +0900

    ✨ (#5) 공통 PrimaryButton 컴포넌트 구현

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,23 @@
 import { Layout } from '../src/components/layout/Layout';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+
+
+import SingUpPage from './pages/Signup/SignUpPage';
 
 export default function App() {
   return (
-    <Layout>
-      <section className="p-6">
-        <h1 className="text-xl font-semibold"></h1>
-        {/* 여기에 실제 화면 내용 */}
-      </section>
-    </Layout>
+    <BrowserRouter>
+      <Layout>
+        <Routes>
+          {/* <Route path="/" element={<Navigate to="/login" replace />} /> */}
+          <Route path="/signup" element={<SingUpPage />} />
+          <Route path="*" element={<div className="p-6">404</div>} />
+        </Routes>
+        <section className="p-6">
+          <h1 className="text-xl font-semibold"></h1>
+          {/* 여기에 실제 화면 내용 */}
+        </section>
+      </Layout>
+    </BrowserRouter>
   );
 }
