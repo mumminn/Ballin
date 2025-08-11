a99e54c77066d39c514f957f991011eb6436d3b4
Haemin Kim <haemin1230@gmail.com>
2025-08-11 09:34:49 +0900

    💄 (#5) 전체 폰트 적용 및 PrimaryButton css 변경

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,9 @@
 import { Layout } from '../src/components/layout/Layout';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { PrimaryButton } from './components/common/PrimaryButton';
 
 
-import SingUpPage from './pages/Signup/SignUpPage';
+// import SingUpPage from './pages/Signup/SignUpPage';
 
 export default function App() {
   return (
@@ -10,11 +11,12 @@ export default function App() {
       <Layout>
         <Routes>
           {/* <Route path="/" element={<Navigate to="/login" replace />} /> */}
-          <Route path="/signup" element={<SingUpPage />} />
-          <Route path="*" element={<div className="p-6">404</div>} />
+          {/* <Route path="/signup" element={<SingUpPage />} /> */}
+          {/* <Route path="*" element={<div className="p-6">404</div>} /> */}
         </Routes>
         <section className="p-6">
           <h1 className="text-xl font-semibold"></h1>
+          <PrimaryButton children="2025"/>
           {/* 여기에 실제 화면 내용 */}
         </section>
       </Layout>
--- frontend/src/components/common/PrimaryButton.tsx
+++ frontend/src/components/common/PrimaryButton.tsx
@@ -3,7 +3,7 @@ type ButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement>;
 export function PrimaryButton({children, ...props}:ButtonProps) {
     return(
         <button
-            className="w-full rounded-md px-4 py-2 bg-[#FFEDAD] text-black disabled:opacity-50 disabled:cursor-not-allowed"
+            className="w-full rounded-2xl px-4 py-3 bg-[#FFEDAD] text-black disabled:opacity-50 disabled:cursor-not-allowed"
                 {...props}
             > 
                 {children}
