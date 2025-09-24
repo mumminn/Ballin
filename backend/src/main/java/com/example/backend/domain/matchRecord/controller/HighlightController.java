bfa3e1eb7d044dac39b652cee34891db321ba2be
Haemin Kim <haemin1230@gmail.com>
2025-09-24 17:55:58 +0900

    :recycle: (#51) url ResponseBody로 보내주는 방식으로 변경

--- backend/src/main/java/com/example/backend/domain/matchRecord/controller/HighlightController.java
+++ backend/src/main/java/com/example/backend/domain/matchRecord/controller/HighlightController.java
@@ -1,10 +1,12 @@
 package com.example.backend.domain.matchRecord.controller;
 
 import com.example.backend.domain.matchRecord.service.HighlightService;
+import com.example.backend.global.api.ApiResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Map;
 import java.util.UUID;
 
 @RestController
@@ -15,9 +17,8 @@ public class HighlightController {
     private final HighlightService highlightService;
 
     @GetMapping("/{recordId}")
-    public ResponseEntity<Void> toFirstYoutube (@PathVariable("recordId") UUID recordId) {
+    public ResponseEntity<ApiResponse<Map<String, String>>> toFirstYoutube (@PathVariable("recordId") UUID recordId) {
         String url = highlightService.resolveYoutubeUrl(recordId);
-        return ResponseEntity.status(302)
-                .header("Location", url).build();
+        return ResponseEntity.ok(ApiResponse.ok(Map.of("url", url)));
     }
 }
